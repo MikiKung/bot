432d316bd918990cd95853b0cb0e849153eeb84b
peeranutHub <peeralove0@gmail.com>
2021-07-25 16:34:05 +0700

    งง4

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 const path = require('path')
 
 module.exports = {
-  basePath: '/docs',
+  assetPrefix: '/bot',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
